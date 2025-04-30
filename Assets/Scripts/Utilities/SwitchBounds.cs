007036f591de88609761b5037bccd9fb6ae49455
PoTiin <573031461@qq.com>
2025-04-30 16:37:45 +0800

    zl250430

---
+++ Assets/Scripts/Utilities/SwitchBounds.cs
@@ -0,0 +1,23 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBounds : MonoBehaviour
+{
+    //TODO:切换场景后调用
+    private void Start()
+    {
+        SwitchConfinerShape();
+    }
+    private void SwitchConfinerShape()
+    {
+        PolygonCollider2D confinerShape = GameObject.FindGameObjectWithTag("BoundsConfiner").GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner confiner = GetComponent<CinemachineConfiner>();
+
+        confiner.m_BoundingShape2D = confinerShape;
+        //Call this if the bounding shape's points change at runtime
+        confiner.InvalidatePathCache();
+    }
+}
